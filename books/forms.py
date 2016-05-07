13ad2360712a73fc4ebf1a5725d990159cb8cbb5
yinpeng <jianglixiaobai@sina.com>
2016-05-07 15:02:21 +0800

    form 自定义校验规则 contact_form.html 的error提示信息显示

--- books/forms.py
+++ books/forms.py
@@ -7,3 +7,15 @@ class ContactForm(forms.Form):
     email = forms.EmailField(required=False)
     message = forms.CharField(widget=forms.Textarea)
 
+    def clean_message(self):
+        '''
+           Django的form系统自动寻找匹配的函数方法，
+           该方法名称以clean_开头，并以字段名称结束。
+           如果有这样的方法，它将在校验时被调用
+        '''
+        message = self.cleaned_data['message']
+        num_words = len(message.split())
+        if num_words<4:
+            raise forms.ValidationError("Not enough words")
+        return message
+
